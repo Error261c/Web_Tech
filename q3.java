7c915112c8b7d87e2062d6afc67f1625422cde65
Error261c <113609202+Error261c@users.noreply.github.com>
2022-09-15 18:38:41 +0530

    Update q3.java

--- q3.java
+++ q3.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Enter the value of num1 and num2");  
+	System.out.println("Enter the value of num1 and num2");  
         Scanner sc = new Scanner(System.in);  
         int num1 = sc.nextInt();  
         int num2 = sc.nextInt();  
